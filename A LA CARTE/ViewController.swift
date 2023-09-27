e952ce1c2d0c6d3d0cbf4978d045618be01d19c6
玉城航汰 <snake@Tamaki.local>
2023-09-27 11:16:30 +0900

    first commit

---
+++ A LA CARTE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A LA CARTE
+//
+//  Created by 玉城航汰 on 2023/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
