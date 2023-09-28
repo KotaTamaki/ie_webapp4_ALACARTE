7f4c56d79937aa5ee87c59ebb3352c1d00ba802c
玉城航汰 <snake@Tamaki.local>
2023-09-28 11:09:00 +0900

    fixed each genre1

--- A LA CARTE/AppDelegate.swift
+++ A LA CARTE/AppDelegate.swift
@@ -1,8 +1,8 @@
 //
 //  AppDelegate.swift
-//  CustomPickerViewSample
+//  A LA CARTE
 //
-//  Created by Fumiya Tanaka on 2021/07/27.
+//  Created by 玉城航汰 on 2023/09/26.
 //
 
 import UIKit
@@ -30,6 +30,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
+   
+    
+    
+    
 
 
 }
--- A LA CARTE/ViewController.swift
+++ A LA CARTE/ViewController.swift
@@ -1,68 +1,17 @@
-import UIKit
 
-class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
-    
-    @IBOutlet var foodPickerView: UIPickerView! // 食べ物のPickerView
-    @IBOutlet var drinkPickerView: UIPickerView! // 飲み物のPickerView
-    
-    // 飲み物のデータの配列
-    var drinkArray: [String] = [
-        "爽健美茶",
-        "麦茶",
-        "コーラ",
-        "オレンジジュース",
-        "天然水"
-    ]
-    
-    // 食べ物のデータの配列
-    var foodArray: [String] = [
-        "ラーメン",
-        "トマト",
-        "生ハム",
-        "きゅうり",
-        "納豆",
-        "お米"
-    ]
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // データソースの設定
-        drinkPickerView.dataSource = self
-        foodPickerView.dataSource = self
-        
-        // デリゲートの設定
-        drinkPickerView.delegate = self
-        foodPickerView.delegate = self
-    }
+ //  ViewController.swift
+ //  A LA CARTE
+ //
+ //  Created by 玉城航汰 on 2023/09/26.
+ //
+ import UIKit
 
-    // PickerView一つあたりのの列の数
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    // PickerView一つあたりの行の数
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        
-        if pickerView == foodPickerView {
-            return foodArray.count
-        }
-        
-        if pickerView == drinkPickerView {
-            return drinkArray.count
-        }
-        
-        return 0
-    }
+ class ViewController: UIViewController {
 
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        if pickerView == foodPickerView {
-            return foodArray[row]
-        }
-        if pickerView == drinkPickerView {
-            return drinkArray[row]
-        }
-        return nil
-    }
+     override func viewDidLoad() {
+         super.viewDidLoad()
+         // Do any additional setup after loading the view.
+     }
 
-}
+
+ }
